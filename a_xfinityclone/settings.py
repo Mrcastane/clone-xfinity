68e6d81439e999d5331e54078bfad73904b59292
Mrcastane <castanedaorlando871@gmail.com>
2025-05-02 21:19:24 +0100

    Fix static directory for static files

--- a_xfinityclone/settings.py
+++ a_xfinityclone/settings.py
@@ -123,6 +123,7 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
