3900e2d55d0b21b16e4a912cf5be8ec2a696e881
Mrcastane <castanedaorlando871@gmail.com>
2025-05-02 17:44:28 +0100

    Fix static directory for static files

--- a_xfinityclone/settings.py
+++ a_xfinityclone/settings.py
@@ -115,19 +115,17 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_URL = '/static/'  # URL used to serve static files
+STATIC_URL = '/static/'
 
-# Folder where collectstatic will gather all static files for production
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # where collectstatic will put files
 
-# Location(s) where your app/static files are stored during development
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static'),  # where you write your static files
 ]
 
+# Optional but helps with performance
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
